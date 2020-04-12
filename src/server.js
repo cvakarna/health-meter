36e947dc5d208873edfa15fa07bd16b0f66e6156
siva_karna <siva.karna@bizruntime.com>
2020-04-12 18:46:16 +0530

    added engines

--- src/server.js
+++ src/server.js
@@ -2,13 +2,16 @@
 //Install express server
 const express = require('express');
 const app = express();
+const path = require('path');
 
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist'));
 
-app.all('*', (req, res) => {
-    res.status(200).sendFile(__dirname + '/dist/index.html');
+app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
+
+console.log("Express is working");
